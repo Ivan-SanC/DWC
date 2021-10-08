b3e2858e1ccae00044e6c009648094f47e5d4f0c
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-10-08 20:57:19 +0200

    random update

--- Activitats1/generaNumero.js
+++ Activitats1/generaNumero.js
@@ -1,3 +1,13 @@
 //Variables
 var random= parseInt(Math.random()*101);
+var numero;
+
+do{
+
+    numero=prompt("Escribe un numero");
+if(isNaN(numero)){
+   //no es un numero
+}
+}while(numero !=random)
+
 
