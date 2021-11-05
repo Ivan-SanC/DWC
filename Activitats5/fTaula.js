6a675837836a6939175c8b97e67c401d5dfa062c
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-11-05 19:28:19 +0100

    corregido

--- Activitats5/fTaula.js
+++ Activitats5/fTaula.js
@@ -1,10 +1,10 @@
-function createTable (filas=10, columnas=4, color="black"){
+function creaTabla (filas=10, columnas=4, color="black"){
    
-    document.write(`<table style="border: 3px solid ${color}">`);
+    document.write(`<table style="border: 3px solid ${color}";>`);
         for (let i = 0; i < filas; i++) {
             document.write(`<tr style="border: 1px solid ${color}">`);
                 for (let j = 0; j < columnas; j++ ) {
-                    document.write(`<th style="border: 1px solid ${color}"></th>`);
+                    document.write(`<td style="border: 1px solid ${color}"></td>`);
             
                 }
 
@@ -13,8 +13,8 @@ function createTable (filas=10, columnas=4, color="black"){
 
     document.write("</table>");
 }
-createTable();
-createTable(20,10);
+creaTabla();
+creaTabla(20,10);
 for (let i = 0; i < 10; i++) {
-    createTable(5,4,"green");
+    creaTabla(5,4,"green");
 }
