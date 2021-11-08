f1f9f00754af7df0bb81b3376e14d369e8d3f1f9
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-11-08 16:23:54 +0100

    up

--- Activitats5/fPrimo.js
+++ Activitats5/fPrimo.js
@@ -1,6 +1,6 @@
 function esPrimo(numero) {
     
-    for(let i = 2,raiz=Math.sqrt(numero); i <= raiz; i++)
+    for(let i = 2; i*i <= numero; i++)
         if(numero % i === 0) return false;
     return numero > 1;
 }  
