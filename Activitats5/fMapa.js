c0c705c44ec75fbf888a8f6f134a1f7775e55aea
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-11-08 17:50:18 +0100

    map

---
+++ Activitats5/fMapa.js
@@ -0,0 +1,41 @@
+//Variables
+var array=[];
+var palabras;
+var map= new Map();
+var existe;
+
+//introduce palabras en el array
+do{
+palabras=prompt("Introduce palabras.")
+if(palabras!=null && palabras!=""){
+    array.push(palabras);
+
+}
+
+}while(palabras!=null && palabras!="");
+
+
+//Crea el mapa
+function contador(arrayPalabras){
+    let valor=1;
+
+    for(let i=0;i<arrayPalabras.length;i++){
+
+        //mira que exista la palabra en el map
+        if(map.has(arrayPalabras[i])==true){
+            map.set(arrayPalabras[i],valor+1);
+        
+        //Si no existe lo crea
+        }else{
+            map.set(arrayPalabras[i],valor);
+        }
+        
+    }
+
+    return map;
+}
+
+//Muestra la clave y el valor del mapa
+for (let [clave,valor] of contador(array)) {
+    document.write(`Clave: ${clave}, Valor: ${valor} <br>`);
+}
