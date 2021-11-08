07f21d867431d8ca41d414e6d1322adeeb3a9830
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-11-08 16:48:13 +0100

    up

--- Activitats5/fInverso.js
+++ Activitats5/fInverso.js
@@ -1,16 +1,15 @@
 var palabras;
+var array=[];
 const lista= new Set();
 do{
     palabras=prompt("Introduce palabras");
     if(palabras!=null){
         lista.add(palabras);
     }
-}while(palabras!=null);
+}while(palabras!=null && palabras!="");
 
-const array=[...lista];
-function ordena(a,b){
-    return a.length-b.length;
-}
+array=[...lista];
+
 
 array.sort((a,b)=>a.localeCompare(b,'es'));
 const reversed=array.reverse();
