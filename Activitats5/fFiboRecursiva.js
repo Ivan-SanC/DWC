c15da5f10f8583278485ce990468a6f60745135a
Ivan-SanC <ivan.s.c@hotmail.com>
2021-11-10 00:07:36 +0100

    fiba

--- Activitats5/fFiboRecursiva.js
+++ Activitats5/fFiboRecursiva.js
@@ -1,5 +1,21 @@
-function fiboRe(num){
-    let num1=0;
-    let cont=0;
-    if(cont>=num)
+function fibonacci(num) {
+    if(num < 2) {
+        return num;
+
+    }else {
+        return fibonacci(num-1) + fibonacci(num - 2);
+    }
+}
+do{
+    var esValido=true;
+    valor=prompt("Introduce un valor.");
+    if(isNaN(Number(valor))){
+        alert("No es un numero");
+        esValido=false;
+    }
+
+}while(esValido==false);
+
+for(let i = 0; i < valor; i++) {
+    document.write(`${fibonacci(i)} `);
 }
