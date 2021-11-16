fee199133c5e23d55d7192660332032239d2fd2d
Ivan-SanC <ivan.s.c@hotmail.com>
2021-11-17 00:16:55 +0100

    up

--- ActivitatsPoo/punto.js
+++ ActivitatsPoo/punto.js
@@ -1,9 +1,33 @@
 function Punto(coordX, coordY){
     this.x=coordX;
     this.y=coordY;
-    this.cambiar=(coordx,coordY)=> this.x=coordx; this.y=coordY;
-    this.copia=()=>this.Punto;
-    this.obtenerDistancia=(punto2)=>
-    this.sumar=(punto2)=>
-    this.toString=()=> `(${this.x},${this.y})`;
+
+    this.cambiar=(coordX,coordY)=> this.x=coordX; this.y=coordY;
+
+    //this.copia=()=>this;
+
+    //this.obtenerDistancia=(punto2)=>Math.sqrt(Math.pow(Math.abs(this.x-(punto2.x)),2)+(Math.pow(Math.abs(this.y-(punto2.y)),2)));
+
+    //this.suma=(punto2)=> punto2.x+this.x; punto2.y+this.y
+
+    this.toString=()=> (this.x +","+ this.y);
 }
+
+var p = new Punto(1,2); 
+console.log("p: " + p.toString()) // Salida: (1,2)
+var q = new Punto(6,-3);
+console.log("q: " + q.toString()); //Salida: (6,-3)
+
+p.cambiar(-5,2);
+
+var r = p.copia();
+console.log("r: " + r.toString());
+r.x = 9;
+
+console.log("p: " + p.toString()); // Salida: (-5,2)
+console.log("r: " + r.toString()); // Salida: (9,2)
+
+var s = p.suma(r);
+console.log("s: " + s.toString()); // Salida: (4,4)
+
+console.log("Distancia entre p y q: " + p.obetenerDistancia(q)); // Salida: 12.08
