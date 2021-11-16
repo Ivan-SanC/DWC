b53317a3b657ff7715958881e17c135edcea99ff
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-11-16 19:18:21 +0100

    proceso punto

---
+++ ActivitatsPoo/punto.js
@@ -0,0 +1,9 @@
+function Punto(coordX, coordY){
+    this.x=coordX;
+    this.y=coordY;
+    this.cambiar=(coordx,coordY)=> this.x=coordx; this.y=coordY;
+    this.copia=()=>this.Punto;
+    this.obtenerDistancia=(punto2)=>
+    this.sumar=(punto2)=>
+    this.toString=()=> `(${this.x},${this.y})`;
+}
