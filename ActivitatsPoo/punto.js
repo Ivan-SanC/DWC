2ca06e25bcd0fb80ecdc0024da551a200e7f0dca
Ivan-SanC <ivan.s.c@hotmail.com>
2021-11-17 17:28:13 +0100

    update

--- ActivitatsPoo/punto.js
+++ ActivitatsPoo/punto.js
@@ -2,14 +2,22 @@ function Punto(coordX, coordY){
     this.x=coordX;
     this.y=coordY;
 
-    this.cambiar=(coordX, coordY)=> this.x=coordX ; this.y=coordY;
+    //this.cambiar=(coordX, coordY)=> this.x=coordX; this.y=coordY;
+    this.cambiar=function (coordX, coordY){ 
+        this.x=coordX; 
+        this.y=coordY;
+    }
 
     this.copia=()=> this;
 
     this.obtenerDistancia=(punto2)=>Math.sqrt(Math.pow(Math.abs(this.x-(punto2.x)),2)+(Math.pow(Math.abs(this.y-(punto2.y)),2)));
 
     //this.sumar=(punto2)=> punto2.x+this.x; punto2.y+this.y;
-
+    this.sumar=function(punto2){
+        this.x=punto2.x+this.x; 
+        this.y=punto2.y+this.y;
+        return this;
+    }
     this.toString=()=> `(${this.x},${this.y})`;
 }
 
@@ -30,4 +38,4 @@ console.log("r: " + r.toString()); // Salida: (9,2)
 var s = p.sumar(r);
 console.log("s: " + s.toString()); // Salida: (4,4)
 
-console.log("Distancia entre p y q: " + p.obetenerDistancia(q)); // Salida: 12.08
+console.log("Distancia entre p y q: " + p.obtenerDistancia(q)); // Salida: 12.08
