f732d52bad69b0e9c7d40f77fa2272d2c5759710
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-11-22 17:56:40 +0100

    mostrar propietarios for clave valor

--- SanchezIvan_ComunitatPropietaris/edificio.js
+++ SanchezIvan_ComunitatPropietaris/edificio.js
@@ -8,15 +8,107 @@ function Edificio(tipoVia, nombreVia, numeroEdificio, codigoPostal){
     this.mapaPropiertariosEdificio=new Map();
 
     //Metodos
+
+    //Agregar
     this.agregarPlanta=function(numeroPlanta){
-        this.numeroPlanta=numeroPlanta;
+        this.mapaPropiertariosEdificio.set(numeroPlanta,new Map());
     }
+
+    this.agregarPuerta=function(numeroPlanta,numeroPuerta){
+        this.mapaPropiertariosEdificio.get(numeroPlanta).set(numeroPuerta,new Array());
+    }
+
     this.agregarPropietario=function(nombrePropietario,numeroPlanta,numeroPuerta){
-        this.nombrePropietario=nombrePropietario;
-        this.numeroPlanta=numeroPlanta;
-        this.numeroPuerta=numeroPuerta;
+        this.mapaPropiertariosEdificio.get(numeroPlanta).get(numeroPuerta).push(nombrePropietario);
     }
 
-    
 
+    //Modificar
+    this.modificarTipoVia=function(nuevoTipoVia){
+        this.tipoVia=nuevoTipoVia;
+    }
+
+    this.modificrNombreVia=function(nuevoNombreVia){
+        this.nombreVia=nuevoNombreVia
+    }
+
+    this.modificarNumeroEdificio=function(nuevoNumeroEdificio){
+        this.numeroEdificio=nuevoNumeroEdificio;
+    }
+
+    this.modificarCodigoPostal=function(nuevoCodigoPostal){
+        this.codigoPostal=nuevoCodigoPostal;
+    }
+
+
+    //Imprimir
+    this.imprimirTipoVia=()=> "imprimirTipoVia: "+this.tipoVia;
+
+    this.imprimirNombreVia=()=>"imprimirNombreVia: "+this.nombreVia;
+
+    this.imprimirNumeroEdificio=()=>"imprimirNumeroEdificio: "+this.numeroEdificio;
+    
+    this.imprimirCodigoPostal=()=>"imprimirCodigoPostal: "+this.codigoPostal;
+    
+    this.imprimirTodosPropietarios=function(){
+        for (let [clave,valor] of this.mapaPropiertariosEdificio) {
+            `Planta: ${clave}
+                    Puerta: ${valor}`;
+                    for(let i=0;i<){
+
+                    }
+        }
+    
 }
+const edificio1 = new Edificio('calle', 'marques de la fontsanta', 60, '07005');
+
+edificio1.agregarPlanta('1A');
+edificio1.agregarPlanta('1B');
+
+edificio1.agregarPuerta('1A','3');
+edificio1.agregarPuerta('1A','2');
+edificio1.agregarPuerta('1B','1')
+
+edificio1.agregarPropietario('Belén Laserna Belenguer','1A','3');
+edificio1.agregarPropietario('Pedro Jimenez Vázquez','1A','3');
+edificio1.agregarPropietario('Ana María Rodriguez Figuerola', '1A','2');
+edificio1.agregarPropietario('Miguel López López','1B','1');
+console.log(edificio1);
+/* SALIDA:
+    Edificio {
+    tipoVia: 'calle',
+    nombreVia: 'marques rubio',
+    numeroEdificio: 70,
+    codigoPostal: '07005',
+    mapaPropietariosEdificio: Map(2) {
+        '1A' => Map(2) { '3' => [Array], '2' => [Array] },
+        '1B' => Map(1) { '1' => [Array] }
+    },
+    modificarTipoVia: [Function (anonymous)],
+    modificarNombreVia: [Function (anonymous)],
+    modificarNumeroEdificio: [Function (anonymous)],
+    modificarCodigoPostal: [Function (anonymous)],
+    imprimirTipoVia: [Function (anonymous)],
+    imprimirNombreVia: [Function (anonymous)],
+    imprimirNumeroEdificio: [Function (anonymous)],
+    imprimirCodigoPostal: [Function (anonymous)],
+    agregarPlanta: [Function (anonymous)],
+    agregarPuerta: [Function (anonymous)],
+    agregarPropietario: [Function (anonymous)],
+    imprimirTodosPropietarios: [Function (anonymous)]
+    }
+    }
+*/
+
+console.log(edificio1.imprimirTodosPropietarios());
+/* SALIDA:
+    Planta: 1A
+        Puerta: 3
+            Belén Laserna Belenguer
+            Pedro Jimenez Vázquez
+        Puerta: 2
+            Ana María Rodriguez Figuerola
+    Planta: 1B
+        Puerta: 1
+            Miguel López López
+*/
