4f1ec60f6d3fe1ef22670f921397e732087c01a3
Ivan-SanC <ivan.s.c@hotmail.com>
2021-11-26 02:03:53 +0100

    finalizado

--- SanchezIvan_ComunitatPropietaris/ejercicio3.js
+++ SanchezIvan_ComunitatPropietaris/ejercicio3.js
@@ -73,51 +73,166 @@ let nombreVia;
 let numEdf;
 let cp;
 let planta;
+let plantaRepetida;
 let puerta;
+let puertaRepetida;
 let propietario;
 let cancelar=false;
+let expCp= new RegExp('^[0-9]{5}$');
+let expPuerta= new RegExp('-[0-9]')
+let confirmar;
+let continuar;
+let existe=false;
 
 do{
+    let cont=0;
     //preguntar que datos no se deben repetir
-    tipoVia=prompt("Introduce el tipo de vìa:");
+    tipoVia=prompt("Introduce el tipo de vía:");
     if(tipoVia==null||tipoVia==""){
         cancelar=true;
-    }
 
-    nombreVia=prompt("Introduce el nombre de la vía: ");
-    if(nombreVia==null||nombreVia==""){
+    }else{
+       
+        nombreVia=prompt("Introduce el nombre de la vía: ");
+       
+        if(nombreVia==null||nombreVia==""){
         cancelar=true;
-    }
+   
+        }else{
+           
+            numEdf=prompt("Introduce el numero del edificio: ");
+           
+            if(numEdf==null||numEdf==""){
+            cancelar=true;
+           
+            }else{
+            
+                //solo numeros 5max
+                do{
 
-    numEdf=prompt("Introduce el numero del edificio: ");
-    if(numEdf==null||numEdf==""){
-        cancelar=true;
-    }
-    //solo numeros 5max
-        cp=prompt("Introduce el código postal: ");
-    if(cp==null||cp==""){
-        cancelar=true;
+                cp=prompt("Introduce el código postal: ");
+                if(cp==null){
+                    cancelar=true;
+                }
+
+                }while(!expCp.test(cp)&&cancelar==false);
+                
+                if(cancelar==false){
+                   
+                    do{
+                        
+
+                        //repetir hasta cancelar o algo escrito
+                        do{
+                            planta=prompt("Introduce la planta: ");
+                   
+                            if(planta==null){
+                            cancelar=true;
+                            }
+                        }while(planta==""&&cancelar==false);
+
+                        if(cancelar==false){
+                        
+                        //no negativas  vuleve a pedir puerta si existe la puerta?
+                            do{
+                                puertaRepetida=false;
+                                plantaRepetida=false;
+                                puerta=prompt("Introduce la puerta: ");
+                                if(puerta==null){
+                                cancelar=true;
+                                }
+                                
+                                if(continuar==true){
+                                    for (let [edfPlantas,edfPuertas] of edificio1.mapaPropiertariosEdificio) {
+                                        if(planta==edfPlantas){
+                                            plantaRepetida=true;
+                                        }
+                                        for(let[edfPuerta,edfPropietarios]of edfPuertas){
+                                            if(puerta==edfPuerta){
+                                            alert("Puerta Repetida");
+                                            puertaRepetida=true;
+                                            }
+                                        }
+                                    }
+                                }
+                                
+
+                            }while((expPuerta.test(puerta)&&cancelar==false)||(puerta==""&&cancelar==false)||(cancelar==false&&puertaRepetida==true));
+                            if(cancelar==false){
+                            
+                                 //solo letras confirm para otro propietario
+                                do{ 
+                                    do{
+                                        propietario=prompt("Introduce el propietario: ");
+                                        if(propietario==null){
+                                        cancelar=true;
+                                        }
+                                    }while(propietario==""&&cancelar==false);
+                                    
+                                    if(confirmar==true&&cancelar==false){
+                                    edificio1.agregarPropietario(propietario,planta,puerta);
+                                    }
+                                    
+                                        if(cancelar==false){
+                                
+                                        if(cont==0){
+                                            var edificio1=new Edificio(tipoVia,nombreVia,numEdf,cp);
+                                            edificio1.agregarPlanta(planta);
+                                            edificio1.agregarPuerta(planta,puerta);
+                                            edificio1.agregarPropietario(propietario,planta,puerta);
+                                            cont++;
+                                            existe=true;
+
+                                        }else if(cont>0&&confirmar==false){
+                                            if(plantaRepetida==false){
+                                                edificio1.agregarPlanta(planta);
+                                            }
+                                            edificio1.agregarPuerta(planta,puerta);
+                                            edificio1.agregarPropietario(propietario,planta,puerta);
+
+                                        }
+                                    
+                                            confirmar=confirm("¿Quieres introducir otro propietario?");
+                                        if (confirmar==false){
+                                            cancelar=true;
+                                        }
+                                    }
+                                
+                                
+                                }while(cancelar==false);
+                                if(cont!=0){
+                                 alert("Se ha introducido un dato nuevo.");
+                                }
+                            }
+                        }
+                        continuar=false;
+                        continuar=confirm("¿Quieres seguir introduciendo una nueva planta o nueva puerta?");
+                        if(continuar==true){
+                        cancelar=false;
+                        }
+
+                    }while(continuar==true);
+                }
+            }
+        }
     }
     
-    //no negativas
-    planta=prompt("Introduce la planta: ");
-    if(planta==null||planta==""){
-        cancelar=true;
-    }
-    //no negativas
-    puerta=prompt("Introduce la puerta: ");
-    if(puerta==null||puerta==""){
-        cancelar=true;
-    }
-
-    //solo letras confirm para otro propietario
-    propietario=prompt("Introduce el propietario: ");
-    if(propietario==null||propietario==""){
-        cancelar=true;
-    }
-    if(cancelar==false){
-        alert("Se ha introducido un dato nuevo")
-        //guarda los datos
-        //tipoVia; nombreVia; numEdf; cp; planta; puerta; propietario;
-    }
 }while(cancelar==false);
+
+function randomColor(){
+    var x = parseInt(Math.random() * 256);
+    var y = parseInt(Math.random() * 256);
+    var z = parseInt(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+//Ejecucion
+document.body.style.background = bgColor;
+}
+
+if(existe==true){
+    document.write(`<h1>Comunitat de propietaris<br>${edificio1.imprimirTipoVia()} ${edificio1.imprimirNombreVia()},
+${edificio1.imprimirNumeroEdificio()} C.P ${edificio1.imprimirCodigoPostal()}</h1>${edificio1.imprimirTodosPropietarios()}`);
+    
+//Fondo
+randomColor(); 
+}
