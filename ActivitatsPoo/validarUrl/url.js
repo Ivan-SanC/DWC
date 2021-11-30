aa3d350c277017e9ba93abc39fc1a0c9c66fb9b3
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-11-30 19:15:43 +0100

    up

--- ActivitatsPoo/validarUrl/url.js
+++ ActivitatsPoo/validarUrl/url.js
@@ -1,8 +1,8 @@
 let url=prompt("Introduce una URL");
 
-let expresion= new RegExp('^((http:)(\\/{0,3})){1}([A-Za-z0-9.-]*((\\:{1}.+\\@){1}|\\@{1}))?(([A-Za-z0-9]+((\\.)[A-Za-z0-9]+)){1}(\\:[0-9]*)?)(\\/[A-za-z0-9\\/\\.]+)((\\?).+)$');
+let expresion= new RegExp('^((http:)(\\/){0,3}){1}([A-Za-z0-9.-]*((\\:{1}.+\\@){1}|\\@{1}))?(([A-Za-z0-9]+((\\.)[A-Za-z0-9]+)){1}(\\:[0-9]*)?)(\\/[A-za-z0-9\\/\\.]+)((\\?).+)$');
 
-
+//let expresion=new RegExp('^((http:)(\\/){0,3}){1}$');
 
 if(expresion.test(url)==true){
     alert("URL Válida.")
