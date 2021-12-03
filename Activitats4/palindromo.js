70d7b3133305514d9cc322f97d4a58a05921f672
Ivan-SanC <ivan.s.c@hotmail.com>
2021-12-03 18:53:42 +0100

    up

--- Activitats4/palindromo.js
+++ Activitats4/palindromo.js
@@ -1,5 +1,5 @@
 function palindromeChecker(str) {
-    const newStr = str.replace(/[\W_]/g, "").toLowerCase()
+    const newStr = str.replace(/[" "]/g, "").toLowerCase()
     const strReversed = newStr.split("").reverse().join("")
   
     return newStr === strReversed ? "es palindromo" : "no es palindromo"
