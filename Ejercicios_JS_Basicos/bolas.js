0ed75583abfbbaa4ee6f2dbff28130e40bbfbe59
Ivan Sanchez <ivan.s.c@hotmail.com>
2022-01-11 19:08:11 +0100

    up

--- Ejercicios_JS_Basicos/bolas.js
+++ Ejercicios_JS_Basicos/bolas.js
@@ -1,5 +1,5 @@
 let bola;
-let random=parseInt((Math.random() *(16-5))+5);
+let random=parseInt(Math.random() *(16-5)+5);
 const lista=new Set();
 
 document.write("<h2>Elimina Valors Repetits</h2>");
@@ -7,7 +7,7 @@ document.write("<p>Actualiza la pàgina per a mostrar un nou grup de valors..</p
 document.write(`<p><strong>Entre aquestes ${random} bolles...</strong></p>`);
 
 for(i=1;i<=random;i++){
-    bola="&#"+parseInt((Math.random() *(10112-10102))+10102)
+    bola="&#"+parseInt(Math.random() *(10112-10102)+10102);
     document.write(bola+" ");
     lista.add(bola);
 }
