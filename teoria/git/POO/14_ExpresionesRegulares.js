fd03da5ebc11edb460026ccfa4bccf31c7a9054d
Ivan-SanC <ivan.s.c@hotmail.com>
2022-01-19 03:11:52 +0100

    up

--- teoria/git/POO/14_ExpresionesRegulares.js
+++ teoria/git/POO/14_ExpresionesRegulares.js
@@ -173,3 +173,26 @@ console.log(expresion14.test(cp2)); // Salida: false
     Estas opciones se pueden encontrar en:
         https://www.w3schools.com/js/js_regexp.asp
 */
+
+
+/*
+.               Un punto indica cualquier carácter
+^expresión      El símbolo ^ indica el principio del String. En este caso el String debe contener la expresión al principio.
+expresión$      El símbolo $ indica el final del String. En este caso el String debe contener la expresión al final.
+[abc]           Los corchetes representan una definición de conjunto. En este ejemplo el String debe contener las letras a ó b ó c.
+[abc][12]       El String debe contener las letras a ó b ó c seguidas de 1 ó 2
+[^abc]          El símbolo ^ dentro de los corchetes indica negación. En este caso el String debe contener cualquier carácter excepto a ó b ó c.
+[a-z1-9]        Rango. Indica las letras minúsculas desde la a hasta la z (ambas incluidas) y los dígitos desde el 1 hasta el 9 (ambos incluidos)
+A|B             El carácter | es un OR.  A ó B
+AB              Concatenación. A seguida de B
+
+\d      Dígito. Equivale a [0-9]
+\D      No dígito. Equivale a [^0-9]
+\s      Espacio en blanco. Equivale a [ \t\n\x0b\r\f]
+\S      No espacio en blanco. Equivale a [^\s]
+\w      Una letra mayúscula o minúscula, un dígito o el carácter _
+        Equivale a  [a-zA-Z0-9_]
+\W      Equivale a [^\w]
+\b      Límite de una palabra.
+
+*/
