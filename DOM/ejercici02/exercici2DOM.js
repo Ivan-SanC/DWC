5303289779f39f7567495c86bb61153c1f44c70c
Ivan-SanC <ivan.s.c@hotmail.com>
2022-01-21 23:35:43 +0100

    up

--- DOM/ejercici02/exercici2DOM.js
+++ DOM/ejercici02/exercici2DOM.js
@@ -1,5 +1,19 @@
-function ocultarMostra(paragraf,enlace){
-    document.getElementById(paragraf).className=("ocult");
-    document.getElementById(enlace);
+let idParrafo;
+let idEnlace;
+
+function ocultarMostra(parrafo,enlace){
+
+    idParrafo=document.getElementById(parrafo);
+    idEnlace=document.getElementById(enlace);
+
+    if(idParrafo.className=='visible'){
+        idParrafo.className='ocult';
+        idEnlace.innerHTML='Mostrar';
+    }else{
+        idParrafo.className='visible';
+        idEnlace.innerHTML='Ocultar';
+    }
+
+    
 
 }
