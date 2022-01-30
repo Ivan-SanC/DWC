a96c988a197502909d28f947200eab754fd88bd1
Ivan-SanC <ivan.s.c@hotmail.com>
2022-01-30 13:57:03 +0100

    up

--- DOM/ejercicio04/exercici4DOM.js
+++ DOM/ejercicio04/exercici4DOM.js
@@ -1,7 +1,30 @@
 
-document.getElementById('marcaBici').disabled=false;
-/*function habilitarSelect(identificador){
-    if(identificador=='marcaBici'){
+function habilitarSelect(identificador){
+    var getId=document.getElementById(identificador);
+    
+    if(getId.disabled){
+        getId.disabled=false;
         
+    }else{
+        getId.disabled=true
+        
+    }  
+}
+
+
+
+function retornaValor(elemento){
+    console.log(elemento.value);
+    
+    let contenido=document.createTextNode(elemento.value+" ");
+    document.body.appendChild(contenido);
+}
+
+function validarFormulario(form){
+    let formulario=document.getElementById(form);
+
+    for(element of formulario){
+        retornaValor(element);
     }
-}*/
+    
+}
