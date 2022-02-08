a7cc90a1b2b30b67af68d12ec11571da2c4be644
Ivan-SanC <ivan.s.c@hotmail.com>
2022-02-08 22:48:02 +0100

    up

--- SanchezIvan_TaulesDOM/script.js
+++ SanchezIvan_TaulesDOM/script.js
@@ -1,24 +1,23 @@
 function crearTablaColores(tablaColores,numColores){
     let tabla=document.getElementById(tablaColores);
-    //cambiar por rows
+    //numero de columnas
     let numColumna=tabla.tBodies[0].rows[0].cells.length;
     //let numColumna=tabla.getElementsByTagName('th').length;
     let numFila=document.getElementById(numColores).value;
     
-    //limitar numeros
+    //limitar numeros entre 1-20
     if(numFila>=1&&numFila<=20){
+
         crearTabla(numFila,numColumna);
         console.log(tabla);
+            //generar un mapa con Array con 3 numeros rgb
             let datos=generarNumero(numFila);
-            //añadir funcion datos
-            introducirDatosTabla(tabla,datos);
-            document.getElementById(numColores).disabled=true;
-            document.getElementById(numColores).value="";
-        
-    }
-    //deshabilitar input text al hacer click en crear Tabla
-    
 
+            introducirDatosTabla(tabla,datos);
+            //deshabilita el textbox y lo limpia
+            document.getElementById(numColores).disabled=true;
+            document.getElementById(numColores).value="";   
+    }
 }
 
 //se llama en creartablaColores
@@ -28,49 +27,55 @@ function crearTablaColores(tablaColores,numColores){
 //filas=tr columnas=td
 //opcion insertCells insertRows
 function crearTabla(numFila,numColumna){
-   // let cont=0;
-    var tblBody=document.getElementsByTagName('tbody')[0];
+    let tblBody=document.getElementsByTagName('tbody')[0];
 
+    //for para crear las filas
     for(let i=1;i<=numFila;i++){
-        var fila=document.createElement('tr');
-        //cont++;
+        let fila=document.createElement('tr');
         let columna="";
+
+        //for para crear columnas
         for(let i=1;i<=numColumna;i++){
-            
-             columna=document.createElement('td');
+            columna=document.createElement('td');
             fila.appendChild(columna);
+
         }
         tblBody.appendChild(fila);
+
     }
         return tblBody;
+
 }
 
 
-
+//función para crear un map la key será la fila
+//el array contendra 3 numeros aleatorios para el rgb que son las columnas
 function generarNumero(numFila){
     let map=new Map();
     
+    //bucle para generar las filas
     for(let n=1;n<=numFila;n++){
         let arrayColor=Array();
+
+        //bucle para generar las columnas con el rgb
         for(let i=0; i<3;i++){
             let num=parseInt(Math.random()*256);
             arrayColor.push(num);
         }
-        
+        //se inserta en el map 
         map.set(n,arrayColor);
     }   
     console.log(map);
     return map;
 }
 
+
 //se llama en crearTablaColores
 function introducirDatosTabla(tabla,datos){
 
-    //let numFilas=tabla.getElementsByTagName('tr').length;
-    
-        for (let[posicion,color]of datos){
+    //for of para sacar la posición de la row y el color
+     for (let[posicion,color]of datos){
 
-    //bucle cells? generar num randoms todas las veces
     tabla.tBodies[0].rows[posicion].cells[0].innerHTML =color[0];
     tabla.tBodies[0].rows[posicion].cells[1].innerHTML =color[1];
     tabla.tBodies[0].rows[posicion].cells[2].innerHTML =color[2];
@@ -80,10 +85,9 @@ function introducirDatosTabla(tabla,datos){
     
 }
 
-//intencianvian la posicionde las filas 
+//intenciambian la posicion de las filas 
 function permutarFilas(tablaColores,fila1,fila2){
     let tabla=document.getElementById(tablaColores);
-
     let valor1=document.getElementById(fila1).value;
     let copia=document.getElementById(fila2).value;
 
@@ -92,34 +96,24 @@ function permutarFilas(tablaColores,fila1,fila2){
     let aux=document.getElementsByTagName('tr')[parseInt(copia)+1];
 
    
-
+    //solo cambia posiciones si se han creado columnas y si la posición para cambiar no es 0
     if(tabla.getElementsByTagName('td').length>0 && copia!=0 && valor1!=0){
     
         tabla.tBodies[0].insertBefore(pos2,pos1);
-
         tabla.tBodies[0].insertBefore(pos1,aux);
-
     }
-    //usar el before y 3 variables
-
 }
 
 
 function cambiarFondo(tablaColores,filaFondo){
-    //copiar el value de la ultima cell y modificar el style de la pagina
+    
     let tabla=document.getElementById(tablaColores);
     let elemento=document.getElementById(filaFondo).value;
-    console.log(elemento);
-    let numColumna=tabla.tBodies[0].rows[0].cells;
-    console.log(numColumna.length);
-    console.log(numColumna.length-1);
+    let numColumna=tabla.tBodies[0].rows[0].cells.length;
     
-    let posicion=tabla.tBodies[0].rows[elemento].cells[numColumna.length-1].style.backgroundColor;
-    console.log(posicion);
-    document.body.style.background =posicion;
-
+    
+    let color=tabla.tBodies[0].rows[elemento].cells[numColumna-1].style.backgroundColor;
+    console.log(color);
+    document.body.style.background =color;
 
 }
-
-//deshabilitar cuadro de texto primer boton
-//añadir filas y columnas a la tabla ya existente
