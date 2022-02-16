2e203eabee5ad017a85a85f7c41a3dcd80f5440e
Ivan Sanchez <ivan.s.c@hotmail.com>
2022-02-16 20:56:39 +0100

    up

--- DOM/ejercicio05/script.js
+++ DOM/ejercicio05/script.js
@@ -1,6 +1,12 @@
+//este event solo necesitamos cuando queremos que carge primero el dom y luego el script
+//esta escuchando continuo
+//con un boton no es necesario
 document.addEventListener("DOMContentLoaded", function(event) {
 function invertirParrafos(){
+    //devuelve una collection que no podemos trabajar con ella necesita pasar a array
     let p=document.getElementsByTagName('p');
+    //lo mete todo en un array con la propagacion
+    let listaParrafos=[...document.getElementsByTagName('p')];
     let body=document.body;
     let arrayP=Array();
     
@@ -11,6 +17,12 @@ function invertirParrafos(){
     let invertido=arrayP.reverse();
     console.log(invertido);
     console.log(body);
+    
+    /*
+    for (let parrafo of listaParrafos){
+        documen.body.appendchild(parrafo);
+    }
+    */
 
     for(let i=0; i<p.length;i++){
        body.appendChild(invertido[i]);
--- DOM/ejercicio06/script.js
+++ DOM/ejercicio06/script.js
@@ -2,13 +2,52 @@
 //boton generar activa boton sorteo
 //boton generar 1 numero random lo mete en p div
 //comparar numero result con decimos cambiar color rojo o verde
-let max=99;
+//divContenedor
+let max=5;
 let min=1;
-function decimos(){
-    let numero=parseInt(Math.random() * (max+1 - min))+min
-    let p=document.getElementsByTagName('p');
-    let 
+
+function numeroRandom(max,min){
+    return parseInt(Math.random() * (max+1 - min)+min);
+}
+
+function generarDecimo(divContenedor){
+    //idcontenedor
+    let contenedor=document.getElementById(divContenedor);
+    //muestra los hicos de un nodo es una collection necesita un for
+    console.log(contenedor.children);
+
+
     for(let i=0;i<3;i++){
+        const arrayDecimo=Array();
+
+        let numero=numeroRandom(max,min);
+        let p=document.createElement('p');
+        let contenido=document.createTextNode(numero);
+        arrayDecimo.push(numero);
+        console.log(arrayDecimo);
+        p.appendChild(contenido);
+        contenedor.children[i].appendChild(p);
+        
+        
 
     }
+    let sorteo=document.getElementById('realizarSorteo');
+    sorteo.disabled=false;
+    
+}
+
+function realizarSorteo(){
+    let div=document.getElementById('numeroSorteo');
+
+    let numero=numeroRandom(max,min);
+
+    let p=document.createElement('p');
+    let contenido=document.createTextNode(numero);
+
+    p.appendChild(contenido);
+    div.appendChild(p);
+
+
+
+
 }
